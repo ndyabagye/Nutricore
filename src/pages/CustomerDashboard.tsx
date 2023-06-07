07350f401fcac0cd172e3ba2bb3771c02bfa413a
Ndyabagye Henry <ndyabagyehenrytusi@gmail.com>
2023-06-07 13:54:06 +0300

    remove unused imports

--- src/pages/CustomerDashboard.tsx
+++ src/pages/CustomerDashboard.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
-import Table from '../components/Table';
 import Heading from "../components/Heading";
 import Layout from "../layouts/Layout";
-import {db} from '../firebase';
-import {collection, getDocs} from "firebase/firestore";
-import {toast} from "react-hot-toast";
 import PricingTable from "../components/PricingTable";
 
 const CustomerDashboard = () => {
